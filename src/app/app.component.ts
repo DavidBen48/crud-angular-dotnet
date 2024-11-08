b0134bb003e9960591f7210209dbbfd565e46385
David Ben de Oliveira Vieira <davidbensaxofonista@gmail.com>
2024-11-08 19:05:12 -0300

    inicio do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+// app.component.ts
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularDapperCrud';
+}
